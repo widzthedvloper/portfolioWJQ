fb81fd3574b852b2a3e95c91fb6b6cb059edfafd
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-12-19 15:45:38 -0400

    Complete function to move slide to the left

--- js/slider.js
+++ js/slider.js
@@ -6,14 +6,23 @@ $(document).ready(() => {
   $('#slider .slide:last').insertBefore('#slider .slide:first');
   $(slider).css('margin-left', '-25%');
 
-  function moveRight() {
+  const moveRight = () => {
     slider.animate({
       marginLeft: '-75%',
     }, 700, () => {
       $('#slider .slide:first').insertAfter('#slider .slide:last');
       slider.css('margin-left', '-25%');
     });
-  }
+  };
+
+  const moveLeft = () => {
+    $('#slider .slide:last').insertBefore('#slider .slide:first');
+    slider.css('margin-left', '-75%');
+    slider.animate({
+      marginLeft: '-25%',
+    }, 700);
+  };
 
   btnNext.on('click', moveRight);
+  btnPrev.on('click', moveLeft);
 });
