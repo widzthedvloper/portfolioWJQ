145f4de322c324e02d470c7ae384df5af64dc834
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-12-19 15:31:49 -0400

    Complete function to move slide to the right

--- js/slider.js
+++ js/slider.js
@@ -5,4 +5,15 @@ $(document).ready(() => {
 
   $('#slider .slide:last').insertBefore('#slider .slide:first');
   $(slider).css('margin-left', '-25%');
+
+  function moveRight() {
+    slider.animate({
+      marginLeft: '-75%',
+    }, 700, () => {
+      $('#slider .slide:first').insertAfter('#slider .slide:last');
+      slider.css('margin-left', '-25%');
+    });
+  }
+
+  btnNext.on('click', moveRight);
 });
